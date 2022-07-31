8ab6195584850b633e010f9fba0d3c4f645caa6c
Sangadji999 <Riamariksangadji@gmail.com>
2022-07-31 20:06:34 +0700

    github io

---
+++ kampus.php
@@ -0,0 +1,151 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.98.0">
+    <title>tugas kampus</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.2/examples/carousel/">
+    
+
+<link href="assets/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+
+      .b-example-divider {
+        height: 3rem;
+        background-color: rgba(0, 0, 0, .1);
+        border: solid rgba(0, 0, 0, .15);
+        border-width: 1px 0;
+        box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
+      }
+
+      .b-example-vr {
+        flex-shrink: 0;
+        width: 1.5rem;
+        height: 100vh;
+      }
+
+      .bi {
+        vertical-align: -.125em;
+        fill: currentColor;
+      }
+
+      .nav-scroller {
+        position: relative;
+        z-index: 2;
+        height: 2.75rem;
+        overflow-y: hidden;
+      }
+
+      .nav-scroller .nav {
+        display: flex;
+        flex-wrap: nowrap;
+        padding-bottom: 1rem;
+        margin-top: -1px;
+        overflow-x: auto;
+        text-align: center;
+        white-space: nowrap;
+        -webkit-overflow-scrolling: touch;
+      }
+      .container {
+        margin-top: 15px;
+        padding-top: 15px;
+      }
+    </style>
+
+    
+    <!-- Custom styles for this template -->
+    <link href="carousel.css" rel="stylesheet">
+  </head>
+  <body>
+    
+<header>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.php">PROFIL</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="kampus.php">KAMPUS</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="motto.php">MOTTO </a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button type="button" class="btn btn-outline-warning">Search</button>
+
+
+      </form>
+    </div>
+  </div>
+</nav>
+</header>
+
+<main>
+
+<div  class="container">
+  <div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+
+    <!-- START THE FEATURETTES -->
+
+    <div class="row featurette">
+      <div class="col-md-7 order-md-1">
+        <h2 class="featurette-heading fw-normal lh-1">Kampus</h2>
+        <p class="lead" style="font-size: 17px;">STMIK Akakom yang sekarang bernama Universitas Teknologi Digital Indonesia (UTDI) merupakan sekolah tinggi komputer pertama yang ada di Yogyakarta senantiasa ingin dapat memberikan nuansa IT dan menjadi kampus IT kebanggaan di kota Yogyakarta. UTDI siap meluluskan sarjana D-3 dan S-1 dibidang IT yang berdaya saing tinggi dibidangnya.
+
+Lokasi kampus UTDI berada di Jalan Raya Janti (Majapahit) No. 143, Karang Jambe, Banguntapan, Bantul. Lokasi ini sangat mudah dijangkau dari segala arah. Kampus UTDI dekat dengan bermacam-macam fasilitas umum seperti Bandar Udara Internasional Adisutjipto, terminal bus, pusat perbelanjaan terbesar di Yogyakarta, pusat kegiatan pameran (Jogja Expo Center-JEC) dan akses ke luar kota yang mudah. Lingkungan kampus UTDI sangat kondusif untuk kegiatan belajar mengajar karena tidak berada di tepi jalan utama. </p>
+        
+      </div>
+      <div class="col-md-5 order-md-1">
+        <img src="Landmark_Universitas_Teknologi_Digital_Indonesia_(UTDI).jpg" class="img-rounded" alt="Cinque Terre" width="418" height="400"> 
+      </div>
+      </div>
+
+    
+    <hr class="featurette-divider">
+    </div>
+
+    <!-- /END THE FEATURETTES -->
+
+  </div><!-- /.container -->
+
+
+  <!-- FOOTER -->
+  <footer class="container">
+    <p>&copy; 2017–2022 Company, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+  </footer>
+</main>
+
+
+    <script src="assets/dist/js/bootstrap.bundle.min.js"></script>
+
+      
+  </body>
+</html>
